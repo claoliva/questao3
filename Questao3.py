2329cc98d6698b586d95160329c3c170c01a8590
CLAUDIA ALVES DE OLIVEIRA <claoliva2010@gmail.com>
2024-11-13 17:55:03 -0300

    Update Questao3.py

--- Questao3.py
+++ Questao3.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.slider('Grau de satisfação', 0,100)
 numero=st.slider ('Selecionar um número', min_value=0, max_value=100)
-st.text('Seu número é'+str(numero))
+st.text('Seu número é' +str(numero))
